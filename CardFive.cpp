07a568a914f37db0036d5b87aacd98aa8e975942
fares atef <Fares.atef99@outlook.sa>
2021-12-31 20:46:00 +0200

    cardfive

---
+++ CardFive.cpp
@@ -0,0 +1,24 @@
+#include "CardFive.h"
+
+#include "Player.h"
+
+CardFive::CardFive(const CellPosition& pos) : Card(pos)
+{
+	cardNumber = 5;
+}
+
+CardFive::~CardFive()
+{
+}
+
+
+void CardFive::Apply(Grid* pGrid, Player* pPlayer)
+{
+	Output* pOut = pGrid->GetOutput();
+	Card::Apply(pGrid, pPlayer);
+	pOut->PrintMessage("Move backward with the rolled dice number...");
+
+	pPlayer->Move(pGrid, (-2 * pPlayer->GetjustRolledDiceNum()));
+
+}
+
---
+++ CardFive.h
@@ -0,0 +1,12 @@
+#pragma once
+#include"Card.h"
+
+class CardFive : public Card
+{
+
+public:
+	CardFive(const CellPosition& pos);
+	virtual void Apply(Grid* pGrid, Player* pPlayer);
+	virtual ~CardFive();
+
+};
