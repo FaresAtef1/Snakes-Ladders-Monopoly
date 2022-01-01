e664a1b37d677bda478076d55d8925fb0dfd76e3
Amr Elsheshtawy <sheshtawy321@gmail.com>
2022-01-01 19:24:34 +0200

    Changes to Card 9,10 and 11

--- CardNine.cpp
+++ CardNine.cpp
@@ -61,6 +61,7 @@ void CardNine::Apply(Grid* pGrid, Player* pPlayer)
 					pPlayer->SetWallet(pPlayer->GetWallet() - CardPrice);
 					Owner = pPlayer;
 					pOut->PrintMessage("Purchase complete .");
+					Owned = true;
 				}
 				else
 				{
@@ -72,7 +73,7 @@ void CardNine::Apply(Grid* pGrid, Player* pPlayer)
 			}
 
 		} while (t!="y" && t!="n");
-		Owned = true;
+		
 	} 
 	else 
 	{
